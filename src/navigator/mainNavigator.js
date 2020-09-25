4e9fc4e4b9118ee46db24811ff497f253a3d2282
Crowdbotics <team@crowdbotics.com>
2020-09-25 20:35:17 +0000

    add package blueprint-react-native-notifications-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import NotificationList110423Navigator from '../features/NotificationList110423/navigator';
 import Settings110422Navigator from '../features/Settings110422/navigator';
 import Settings110414Navigator from '../features/Settings110414/navigator';
 import UserProfile110412Navigator from '../features/UserProfile110412/navigator';
@@ -16,6 +17,7 @@ import UserProfile110412Navigator from '../features/UserProfile110412/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+NotificationList110423: { screen: NotificationList110423Navigator },
 Settings110422: { screen: Settings110422Navigator },
 Settings110414: { screen: Settings110414Navigator },
 UserProfile110412: { screen: UserProfile110412Navigator },
